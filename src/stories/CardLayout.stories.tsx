b5da6d20ca0b791353adb5a05ddf8a6c2f9c8f32
janakitti <janakittiratana@gmail.com>
2022-04-18 00:24:56 -0400

    Update CardLayout stories

--- src/stories/CardLayout.stories.tsx
+++ src/stories/CardLayout.stories.tsx
@@ -6,6 +6,8 @@ import Grid from '../components/Structures/Grid/Grid';
 import VSplit from '../components/Structures/VSplit';
 import Card from '../components/Containers/Card/Card';
 import Header1 from '../components/Copy/Header1';
+import VContainer from '../components/Structures/VContainer';
+import Body1 from '../components/Copy/Body1';
 
 export default {
   title: 'Nectar/Composites/CardLayout',
@@ -27,7 +29,24 @@ const Template: ComponentStory<typeof Button> = (args) => (
       <Grid direction={Direction.right}>
         {['One', 'Two', 'Three', 'Four', 'Five'].map((i) => (
           <Card label="">
-            <Header1>{i}</Header1>
+            <VContainer>
+              <Header1 pb={0.5}>This is a card</Header1>
+              <Body1 pb={0.5}>
+                Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem
+                Ipsum has been the industry's standard dummy text ever since the 1500s, when an
+                unknown printer took a galley of type and scrambled it to make a type specimen book.
+                It has survived not only five centuries, but also the leap into electronic
+                typesetting, remaining essentially unchanged. It was popularised in the 1960s with
+                the release of Letraset sheets containing Lorem Ipsum passages, and more recently
+                with desktop publishing software like Aldus PageMaker including versions of Lorem
+                Ipsum.
+              </Body1>
+              <Button
+                type={Level.primary}
+                label="
+                  Click me"
+              />
+            </VContainer>
           </Card>
         ))}
       </Grid>
