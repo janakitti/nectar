1a2165c18d3d2c7a3c530ff6290205898460f79f
janakitti <janakittiratana@gmail.com>
2022-04-24 18:27:38 -0400

    Apply theme to Button stories

--- src/components/Button/Button.stories.tsx
+++ src/components/Button/Button.stories.tsx
@@ -2,13 +2,25 @@ import React from 'react';
 import { ComponentStory, ComponentMeta } from '@storybook/react';
 import Button from './Button';
 import { Level } from '../../shared/PropTypes';
+import { ThemeProvider } from 'styled-components';
+import ThemeContext from '../Utility/ThemeContext';
 
 export default {
   title: 'Nectar/Forms/Button',
   component: Button,
 } as ComponentMeta<typeof Button>;
 
-const Template: ComponentStory<typeof Button> = (args) => <Button {...args} />;
+const Template: ComponentStory<typeof Button> = (args) => (
+  <ThemeContext
+    theme={{
+      colors: {
+        primary0: '#00ff00',
+      },
+    }}
+  >
+    <Button {...args} />
+  </ThemeContext>
+);
 
 export const ButtonPrimary = Template.bind({});
 ButtonPrimary.args = {
--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import '../../styles/main.scss';
 
+import styled from 'styled-components';
 import { Level } from '../../shared/PropTypes';
 
 export interface IButtonProps {
@@ -12,10 +13,19 @@ export interface IButtonProps {
 
 const Button = (props: IButtonProps) => {
   return (
-    <button className={`button ${props.type}`} disabled={props.isDisabled} onClick={props.onClick}>
+    <NectarButton disabled={props.isDisabled} onClick={props.onClick}>
       {props.label}
-    </button>
+    </NectarButton>
   );
 };
+
+const NectarButton = styled.button`
+  border-radius: 1em;
+  padding: 0.5em 1em;
+  font-size: 0.85em;
+  font-family: Poppins;
+  background-color: ${(props) => props.theme.colors?.primary0};
+  transition: 1;
+`;
 
 export default Button;
