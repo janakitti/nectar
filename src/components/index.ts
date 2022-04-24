dc690d801e6008415b3e01ef0717e3f2f04e9aba
janakitti <janakittiratana@gmail.com>
2022-04-24 19:55:53 -0400

    Fix exports

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,1 +1,18 @@
 export { default as Button } from './Button';
+
+export { default as Card } from './Containers/Card';
+
+export { default as Body1 } from './Copy/Body1';
+export { default as Header1 } from './Copy/Header1';
+
+export { default as TextArea } from './Forms/TextArea';
+export { default as TextField } from './Forms/TextField';
+
+export { default as Col } from './Structures/Col';
+export { default as Grid } from './Structures/Grid';
+export { default as Paddable } from './Structures/Paddable';
+export { default as Row } from './Structures/Row';
+export { default as VContainer } from './Structures/VContainer';
+export { default as VSplit } from './Structures/VSplit';
+
+export { default as ThemeContext } from './Utility/ThemeContext';
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,4 @@
-export * from "./components";
+export * from './components';
+export * from './shared/PropTypes';
+export * from './shared/useBreakpoint';
+export * from './theme/theme';
