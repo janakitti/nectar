391d661ad8274aa0fc241903f3cfc1e9a0e4a120
janakitti <janakittiratana@gmail.com>
2022-04-25 19:55:53 -0400

    Add width and height props to Card

--- src/components/Containers/Card/Card.tsx
+++ src/components/Containers/Card/Card.tsx
@@ -5,14 +5,22 @@ import '../../../styles/main.scss';
 
 export interface ICardProps {
   children: React.ReactNode;
+  width?: number;
+  height?: number;
 }
 
-const Card: React.FC<ICardProps> = ({ children }) => {
-  return <NectarCard>{children}</NectarCard>;
+const Card: React.FC<ICardProps> = (props) => {
+  return (
+    <NectarCard width={props.width} height={props.height}>
+      {props.children}
+    </NectarCard>
+  );
 };
 
-const NectarCard = styled.div(
-  ({ theme: { colors, containers } }) => css`
+const NectarCard = styled.div<{ width?: number; height?: number }>(
+  ({ theme: { colors, containers }, width, height }) => css`
+    width: ${width}em;
+    height: ${height}em;
     border-radius: ${containers?.borderRadius}em;
     background-color: ${colors?.light0};
     box-shadow: ${containers?.dropShadow?.hOffset}px ${containers?.dropShadow?.vOffset}px
