5d6223b7175a3908aae34bee3c43e53cecfe1523
janakitti <janakittiratana@gmail.com>
2022-04-25 16:26:39 -0400

    Update TextArea to use theme

--- src/components/Forms/TextArea/TextArea.stories.tsx
+++ src/components/Forms/TextArea/TextArea.stories.tsx
@@ -1,13 +1,19 @@
 import React from 'react';
 import { ComponentStory, ComponentMeta } from '@storybook/react';
 import TextArea from './TextArea';
+import ThemeContext from '../../Utility/ThemeContext';
+import { defaultTheme } from '../../../theme/theme';
 
 export default {
   title: 'Nectar/Forms/TextArea',
   component: TextArea,
 } as ComponentMeta<typeof TextArea>;
 
-const Template: ComponentStory<typeof TextArea> = (args) => <TextArea {...args} />;
+const Template: ComponentStory<typeof TextArea> = (args) => (
+  <ThemeContext theme={defaultTheme}>
+    <TextArea {...args} />
+  </ThemeContext>
+);
 
 export const TextArea1 = Template.bind({});
 TextArea1.args = {
--- src/components/Forms/TextArea/TextArea.tsx
+++ src/components/Forms/TextArea/TextArea.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import '../../../styles/main.scss';
+import styled, { css } from 'styled-components';
 import Paddable from '../../Structures/Paddable/Paddable';
 
 export interface ITextAreaProps {
@@ -14,9 +15,29 @@ export interface ITextAreaProps {
 const TextArea = (props: ITextAreaProps) => {
   return (
     <Paddable top={props.pt} right={props.pr} bottom={props.pb} left={props.pl}>
-      <textarea className="textArea"></textarea>
+      <NectarTextArea />
     </Paddable>
   );
 };
+
+const NectarTextArea = styled.textarea(
+  ({ theme: { colors, containers } }) => css`
+    background-color: ${colors?.light0};
+    padding: ${containers?.fieldPadding}em;
+    border-width: ${containers?.borderWidth}em;
+    border-color: ${colors?.light2};
+    border-radius: ${containers?.borderRadius}em;
+    border-style: solid;
+    font-family: Lato;
+
+    &::placeholder {
+      color: ${colors?.light2};
+    }
+
+    &:focus {
+      outline-color: ${colors?.light2};
+    }
+  `
+);
 
 export default TextArea;
