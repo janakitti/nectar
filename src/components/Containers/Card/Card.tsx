93e691e51efc741b4977f04709fdb1a22db52cb0
janakitti <janakittiratana@gmail.com>
2022-04-25 16:07:58 -0400

    Update Card to use theme

--- src/components/Containers/Card/Card.tsx
+++ src/components/Containers/Card/Card.tsx
@@ -1,4 +1,6 @@
 import React from 'react';
+
+import styled, { css } from 'styled-components';
 import '../../../styles/main.scss';
 
 export interface ICardProps {
@@ -7,7 +9,17 @@ export interface ICardProps {
 }
 
 const Card: React.FC<ICardProps> = ({ label, children }) => {
-  return <div className="card1">{children}</div>;
+  return <NectarCard>{children}</NectarCard>;
 };
+
+const NectarCard = styled.div(
+  ({ theme: { colors, containers } }) => css`
+    border-radius: ${containers?.borderRadius}em;
+    background-color: ${colors?.light0};
+    box-shadow: ${containers?.dropShadow?.hOffset}px ${containers?.dropShadow?.vOffset}px
+      ${containers?.dropShadow?.blur}px rgba(0, 0, 0, ${containers?.dropShadow?.opacity});
+    padding: ${containers?.padding}em;
+  `
+);
 
 export default Card;
