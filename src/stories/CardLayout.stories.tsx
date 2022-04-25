9f74ad2331cf62e6abec03edfa95e7a8e7b9a006
janakitti <janakittiratana@gmail.com>
2022-04-25 19:56:04 -0400

    Update stories

--- src/stories/CardLayout.stories.tsx
+++ src/stories/CardLayout.stories.tsx
@@ -10,6 +10,7 @@ import VContainer from '../components/Structures/VContainer';
 import Body1 from '../components/Copy/Body1';
 import { ThemeContext } from '../components';
 import { defaultTheme } from '../theme/theme';
+import HContainer from '../components/Structures/HContainer';
 
 export default {
   title: 'Nectar/Composites/CardLayout',
@@ -30,24 +31,28 @@ const Template: ComponentStory<typeof Button> = (args) => (
       </div>
       <Grid direction={Direction.right}>
         {['One', 'Two', 'Three', 'Four', 'Five'].map((i) => (
-          <Card>
+          <Card width={20}>
             <VContainer>
-              <Header1 pb={0.5}>This is a card</Header1>
+              <Header1>This is a card</Header1>
               <Body1 pb={0.5}>
                 Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem
                 Ipsum has been the industry's standard dummy text ever since the 1500s, when an
                 unknown printer took a galley of type and scrambled it to make a type specimen book.
                 It has survived not only five centuries, but also the leap into electronic
-                typesetting, remaining essentially unchanged. It was popularised in the 1960s with
-                the release of Letraset sheets containing Lorem Ipsum passages, and more recently
-                with desktop publishing software like Aldus PageMaker including versions of Lorem
-                Ipsum.
+                typesetting.
               </Body1>
-              <Button
-                type={Level.primary}
-                label="
-                  Click me"
-              />
+              <HContainer>
+                <Button
+                  type={Level.primary}
+                  label="
+                  Try me"
+                />
+                <Button
+                  type={Level.secondary}
+                  label="
+                  Cancel"
+                />
+              </HContainer>
             </VContainer>
           </Card>
         ))}
