603ad4cb50478a54dc12bf9f7ee15810a6b8fbcd
janakitti <janakittiratana@gmail.com>
2022-04-25 15:53:02 -0400

    Update theme object and default theme

--- src/theme/styled.d.ts
+++ src/theme/styled.d.ts
@@ -7,10 +7,46 @@ export interface IThemeColors {
   secondary0?: string;
   secondary1?: string;
   secondary2?: string;
+  warn0?: string;
+  warn1?: string;
+  warn2?: string;
+  error0?: string;
+  error1?: string;
+  error2?: string;
+  success0?: string;
+  success1?: string;
+  success2?: string;
+  disabled?: string;
+  black?: string;
+  white?: string;
+  light0?: string;
+  light1?: string;
+  light2?: string;
+  dark0?: string;
+  dark1?: string;
+  dark2?: string;
+}
+
+export interface IThemeContainers {
+  borderWidth?: number;
+  borderRadius?: number;
+  dropShadow?: {
+    hOffset: number;
+    vOffset: number;
+    blur: number;
+    spread: number;
+    color: string;
+  };
+  padding?: number;
+  fieldPadding?: number;
 }
 
 declare module 'styled-components' {
   export interface DefaultTheme {
     colors?: IThemeColors;
+    containers?: IThemeContainers;
+    transitions?: {
+      hover?: number;
+    };
   }
 }
--- src/theme/theme.ts
+++ src/theme/theme.ts
@@ -2,6 +2,28 @@ import { DefaultTheme } from 'styled-components';
 
 export const defaultTheme: DefaultTheme = {
   colors: {
-    primary0: '#ff0000',
+    primary0: '#e1bd00',
+    primary1: '#c8a800',
+    primary2: '#c8a800',
+    white: '#ffffff',
+    light0: '#f6f6f6',
+    light1: '#ececec',
+    light2: '#d7d7d7',
+  },
+  containers: {
+    padding: 1,
+    fieldPadding: 0.35,
+    borderRadius: 0.25,
+    borderWidth: 0.16,
+    dropShadow: {
+      hOffset: 0,
+      vOffset: 4,
+      blur: 20,
+      spread: 0,
+      color: '#000000',
+    },
+  },
+  transitions: {
+    hover: 0.15,
   },
 };
