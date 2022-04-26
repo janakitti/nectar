37a732bc4e52663e4e4b208e496de93c00fc8e4c
janakitti <janakittiratana@gmail.com>
2022-04-25 20:56:01 -0400

    Add alignItems and justifyContent props to VContainer

--- src/components/Structures/VContainer/VContainer.tsx
+++ src/components/Structures/VContainer/VContainer.tsx
@@ -1,12 +1,28 @@
 import React from 'react';
 import '../../../styles/main.scss';
+import styled, { css } from 'styled-components';
 
 export interface IVContainerProps {
   children: React.ReactNode;
+  alignItems?: string;
+  justifyContent?: string;
 }
 
 const VContainer = (props: IVContainerProps) => {
-  return <div className="vcontainer">{props.children}</div>;
+  return (
+    <NectarVContainer alignItems={props.alignItems} justifyContent={props.justifyContent}>
+      {props.children}
+    </NectarVContainer>
+  );
 };
+
+const NectarVContainer = styled.div<{ alignItems?: string; justifyContent?: string }>(
+  ({ alignItems, justifyContent }) => css`
+    display: flex;
+    flex-direction: column;
+    align-items: ${alignItems};
+    justify-content: ${justifyContent};
+  `
+);
 
 export default VContainer;
